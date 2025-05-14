5e7a00d0a99314c9f2c8928f357f4103d2ada2fb
bansi <bansidobariya2510@gmail.com>
2025-05-14 13:33:06 +0100

    db conncetion added

---
+++ src/app/components/Navigation.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/cabins">Cabin</Link>
+        </li>
+        <li>
+          <Link href="/about">about</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import Navigation from "./components/Navigation";
 
-export default function Home() {
-  return <div className={styles.page}>hi</div>;
+export default function Page() {
+  return (
+    <div>
+      hi
+      <Navigation />
+      <Link href="/cabins">Cabin</Link>
+    </div>
+  );
 }
